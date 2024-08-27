e2ac0ba2dbedeb56cd42ad35e29023d6746514cf
Sergey-Yar <ssergant@mail.ru>
2024-08-27 21:37:22 +0300

    Кортеж

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = 5, 665.3, True, 'Mamont'
+print(immutable_var)
+#immutable_var[3] = 4432 значения элементов кортежа нельзя изменить, потому что кортеж относится к неизменяемым типам данных (неизменяемая упорядоченная коллекция)
+mutable_list = ['Food', 554, False, 12.5]
+mutable_list[0] = 123
+mutable_list[1] = 'Good'
+print(mutable_list)
+
+
